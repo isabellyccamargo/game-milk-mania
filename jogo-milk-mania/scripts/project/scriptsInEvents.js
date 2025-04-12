e8c41aabe448c04846bba239f7d98500e0e2dd86
isabellyccamargo <camargoisa114@gmail.com>
2025-04-11 23:19:32 -0300

    adicionando arquivos do jogo exportado

---
+++ jogo-milk-mania/scripts/project/scriptsInEvents.js
@@ -0,0 +1,26 @@
+
+
+const scriptsInEvents = {
+
+	async EventoFase1_Event48_Act2(runtime, localVars)
+	{
+		runtime.goToLayout("game over");
+	},
+
+	async EventoFase2_Event19_Act2(runtime, localVars)
+	{
+		
+	},
+
+	async EventoFase2_Event47_Act2(runtime, localVars)
+	{
+		runtime.goToLayout("vitória")
+	},
+
+	async EventoFase2_Event48_Act2(runtime, localVars)
+	{
+		runtime.goToLayout("game over");
+	}
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
